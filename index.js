1e6ce548cf38c81301c8ed5d082a33ef5ccc866c
supriyas-devops <supriyarupnar99999@gmail.com>
2025-12-11 09:30:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Node App Running on Kubernetes!");
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
