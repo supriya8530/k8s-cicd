ba76932a92dd28b117e96f4b07984f4988d80e61
supriyas-devops <supriyarupnar99999@gmail.com>
2025-12-11 10:40:18 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Node App Running on Kubernetes!");
+    res.send("Node App Running on  the Kubernetes!");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
